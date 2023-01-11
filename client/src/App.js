f17428a6f57f6e421c94072c2da512a75dabb04b
HarmanSuri <harmansuri@hotmail.com>
2023-01-11 13:53:13 -0500

    Create table where rows can be added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 
+
 function App() {
+  const testArr = [1, 2, 3, 4, 5];
+  const [inputArr, setInputArr] = useState([]);
+
+  const [inputData, setInputData] = useState({
+    sets:""
+  });
+  
+  useEffect(
+    () => {
+      console.log(inputArr);
+    }, [inputArr]
+  )
+
+  function changeHandler(e) {
+    setInputData({...inputData, [e.target.name]:e.target.value})
+  }
+  
+  function onClick() {
+    if (inputData.sets !== '') {
+      setInputArr([...inputArr, inputData.sets]);
+      console.log(inputArr);
+      setInputData({sets:""});
+    }
+  }
 
   const [backendData, setBackendData] = useState([{}]);
 
@@ -23,7 +48,29 @@ function App() {
       ): (
         <p>{backendData.count}</p>
       )}
+      <input type='text' name='sets' value={inputData.sets} onChange={changeHandler}/>
+      <button type='button' onClick={onClick}>Click</button>
 
+      <table border={1}>
+        <thead>
+          <tr>
+            <td>Sets</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            inputArr.map(
+              (ele, i) => {
+                return(
+                  <tr>
+                    <td key={i}><input type='text' value={ele} readOnly></input></td>
+                  </tr>
+                )
+              }
+            )
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
